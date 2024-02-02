6324596b2ed32c2fb2bb64fab3f01978bd8aacd1
f3lip3-almeida <157630650+f3lip3-almeida@users.noreply.github.com>
2024-02-02 11:15:27 -0300

    atualizado

---
+++ src/model/Ave.ts
@@ -0,0 +1,21 @@
+class Ave extends Animal{
+    
+    private envergadura: number;
+
+    constructor(_raca:string,
+        _nome: string,
+        _idade: number,
+        _genero: string,
+        _envergadura: number){
+            super(_nome, _idade, _genero);
+            this.envergadura = _envergadura;
+        }
+
+        public getEnvergadura(): number {
+            return this.envergadura;
+        }
+
+        public setEnvergaruda(envergadura:number):void{
+            this.envergadura = envergadura;
+        }
+    }
---
+++ src/model/Reptil.ts
@@ -0,0 +1,20 @@
+class Reptil extends Animal{
+
+    private tipo_de_escamas: string;
+
+    constructor(_tipo_de_escamas: string,
+        _nome: string,
+        _idade: number,
+        _genero: string){
+            super( _nome , _idade ,  _genero);
+            this.tipo_de_escamas = _tipo_de_escamas;
+        }
+
+        public getRaca(): string{
+            return this.tipo_de_escamas;
+        }
+
+        public setRaca(raca:string):void{
+            this.tipo_de_escamas;
+        }
+    }
