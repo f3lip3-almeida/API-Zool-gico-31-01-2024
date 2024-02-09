1fa677e12ceb02e3a9477fd46df99e21fb6476cc
f3lip3-almeida <157630650+f3lip3-almeida@users.noreply.github.com>
2024-02-09 10:29:01 -0300

    Criação do server.post do - (atracao), (zoologico) e (habitat)

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,10 @@ import cors from 'cors';
 import { Ave } from './model/Ave';
 import { Mamifero } from './model/Mamifero';
 import { Reptil } from './model/Reptil';
+import { Habitat } from './model/Habitat';
+import { constants } from 'buffer';
+import { Atracao } from './model/Atracao';
+import { Zoologico } from './model/Zoologico';
 
 const server = express();
 const port: number = 3000;
@@ -21,6 +25,27 @@ server.post('/ave', (req, res) => {
     const { nome, especie, idade, genero, envergadura } = req.body;
     let ave: Ave = new Ave(nome, especie, idade, genero, envergadura);
     res.json(["A nova ave do zoológico é: ", ave]);
+});
+
+server.post(' /Habitat', (req, res) => {
+    const { nome, animais } = req.body;
+    const habitat = new Habitat(nome, animais);
+    console.log(habitat);
+    res.status(200).json('Habitat criado');
+});
+
+server.post('/atracao', (req, res) => {
+    const { nome, habitat } = req.body;
+    const atracao = new Atracao(nome, habitat);
+    console.log(atracao);
+    res.status(200).json('Atração criada');
+});
+
+server.post('/zoologico', (req, res) => {
+    const { nome, atracao } = req.body;
+    const zoo = new Zoologico(nome, atracao);
+    console.log(zoo);
+    res.status(200).json('Zoológico criado');
 });
 
 server.listen(port, () => {
