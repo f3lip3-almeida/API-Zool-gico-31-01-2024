e54850d5b49e480c61ffac22eef2d6d30ec791da
f3lip3-almeida <157630650+f3lip3-almeida@users.noreply.github.com>
2024-02-16 14:17:09 -0300

    ADICIONANDO O COMENTARIO DA CLASSE (Ave.ts), QUE EU ESQUECI DE COMENTAR - E CONCLUÇÃO DO ISSUE - 16/02/2024

--- src/model/Ave.ts
+++ src/model/Ave.ts
@@ -1,8 +1,19 @@
+/** 
+ * Representa uma classe para aves, que são animais com penas e geralmente capazes de voar.
+ */ 
 import { Animal } from "./Animal";
 
 export class Ave extends Animal {
-    private envergadura: number;
+    private envergadura: number; // Envergadura da ave
 
+    /**
+     * Construtor da classe Ave.
+     * @param _nome O nome da ave.
+     * @param _especie A espécie da ave.
+     * @param _idade A idade da ave.
+     * @param _genero O gênero da ave.
+     * @param _envergadura A envergadura da ave.
+     */
     constructor(_nome: string,
         _especie:string,
         _idade: number,
@@ -12,11 +23,19 @@ export class Ave extends Animal {
             this.envergadura = _envergadura;
         }
 
-        public getEnvergadura(): number {
-            return this.envergadura;
-        }
-
-        public setEnvergaruda(envergadura:number):void{
-            this.envergadura = envergadura;
-        }
+    /**
+     * Retorna a envergadura da ave.
+     * @returns A envergadura da ave.
+     */
+    public getEnvergadura(): number {
+        return this.envergadura;
     }
+
+    /**
+     * Define a envergadura da ave.
+     * @param envergadura A nova envergadura para a ave.
+     */
+    public setEnvergaruda(envergadura:number):void{
+        this.envergadura = envergadura;
+    }
+}
