17a475e15b6ec6d27a1401ed4073d373ffec1cfa
f3lip3-almeida <157630650+f3lip3-almeida@users.noreply.github.com>
2024-02-21 13:44:30 -0300

    Adicionando o retorno para o front-end & Criando função para cadastrar um reptil no banco de dados - TUDO CORRIGIDO E FUNCIONAL - 21/02/2024 - 13:44

--- src/model/Reptil.ts
+++ src/model/Reptil.ts
@@ -1,3 +1,4 @@
+import { globalAgent } from "http";
 import { Animal } from "./Animal";
 import { DatabaseModel } from "./DatabaseModel";
 
@@ -45,23 +46,43 @@ export class Reptil extends Animal {
     public setTipoEscamas(_tipo_escamas: string): void {
         this.tipo_escamas = _tipo_escamas;
     }
-    
+
     static async listarRepteis() {
-    const listaDeRepteis: Array<Reptil> = [];
-    try {
-        const queryReturn = await database.query(`SELECT * FROM reptil WHERE tipo_escamas LIKE 'Escudos'`);
-        queryReturn.rows.forEach(reptil => {
-            listaDeRepteis.push(reptil);
-        });
+        const listaDeRepteis: Array<Reptil> = [];
+        try {
+            const queryReturn = await database.query(`SELECT * FROM  reptil`);
+            queryReturn.rows.forEach(reptil => {
+                listaDeRepteis.push(reptil);
+            });
 
-        // Apenas para testar se a lista veio correta do banco
-        console.log(listaDeRepteis);
+            // só pra testar se a lista veio certa do banco
+            console.log(listaDeRepteis);
 
-        return listaDeRepteis;
-    } catch (error) {
-        console.log('Erro no modelo');
-        console.log(error);
-        return "error";
+            return listaDeRepteis;
+        } catch (error) {
+            console.log('Erro no modelo');
+            console.log(error);
+            return "error";
+        }
     }
-}
+
+    static async cadastrarReptil(reptil: Reptil): Promise<any> {
+        console.log('Começando o cadastro');
+        try {
+            let insertResult = false;
+            await database.query(`INSERT INTO reptil (nome, idade, genero, tipo_de_escamas)
+                VALUES
+                ('${reptil.getNome().toUpperCase()}', ${reptil.getIdade()}, '${reptil.getGenero().toUpperCase()}', '${reptil.getTipoEscamas().toUpperCase()}');
+            `).then((result) => {
+                if(result.rowCount != 0) {
+                    insertResult = true;
+                }
+            });
+            console.log('retornando a função');
+            return insertResult;
+        } catch(error) {
+            console.log('algo errado não está certo');
+            return error;
+        }
+    }
 }
