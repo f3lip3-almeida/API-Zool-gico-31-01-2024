09e5a1bc6f6fa1f9bc68be32aa36879c2aabe285
f3lip3-almeida <157630650+f3lip3-almeida@users.noreply.github.com>
2024-02-21 16:12:39 -0300

    Fazendo os metódos listar e cadastrar para as classes Ave e Mamifero - DESAFIO #2 - 21/02/2024 - ENTREGUE NO PRAZO & FUNCIONAL.

--- src/model/Ave.ts
+++ src/model/Ave.ts
@@ -1,30 +1,40 @@
-/** 
- * Representa uma classe para aves, que são animais com penas e geralmente capazes de voar.
- */ 
 import { Animal } from "./Animal";
+import { DatabaseModel } from "./DatabaseModel";
 
+const database = new DatabaseModel().pool;
+
+/**
+ * Representa uma ave no zoológico.
+ * Estende a classe Animal.
+ */
 export class Ave extends Animal {
-    private envergadura: number; // Envergadura da ave
+    
+    /**
+     * A envergadura da ave (em centímetros).
+     */
+    private envergadura: number;
 
     /**
-     * Construtor da classe Ave.
+     * Cria uma nova instância de Ave.
+     * 
      * @param _nome O nome da ave.
-     * @param _especie A espécie da ave.
      * @param _idade A idade da ave.
      * @param _genero O gênero da ave.
      * @param _envergadura A envergadura da ave.
      */
     constructor(_nome: string,
-        _especie:string,
-        _idade: number,
-        _genero: string,
-        _envergadura: number){
-            super(_nome, _idade, _genero);
-            this.envergadura = _envergadura;
-        }
+                _idade: number,
+                _genero: string,
+                _envergadura: number) {
+        // Chama o construtor da classe pai (Animal)
+        super(_nome, _idade, _genero);
+        // Atribui a envergadura fornecida ao atributo envergadura da ave
+        this.envergadura = _envergadura;
+    }
 
     /**
-     * Retorna a envergadura da ave.
+     * Obtém a envergadura da ave.
+     * 
      * @returns A envergadura da ave.
      */
     public getEnvergadura(): number {
@@ -33,9 +43,46 @@ export class Ave extends Animal {
 
     /**
      * Define a envergadura da ave.
-     * @param envergadura A nova envergadura para a ave.
+     * 
+     * @param _envergadura A envergadura a ser atribuída à ave.
      */
-    public setEnvergaruda(envergadura:number):void{
-        this.envergadura = envergadura;
+    public setEnvergadura(_envergadura: number): void {
+        this.envergadura = _envergadura;
+    }
+
+    static async listarAves() {
+        const listaDeAves: Array<Ave> = [];
+        try {
+            const queryReturn = await database.query(`SELECT * FROM  ave`);
+            queryReturn.rows.forEach(ave => {
+                listaDeAves.push(ave);
+            });
+
+            // só pra testar se a lista veio certa do banco
+            console.log(listaDeAves);
+
+            return listaDeAves;
+        } catch (error) {
+            console.log('Erro no modelo');
+            console.log(error);
+            return "error";
+        }
+    }
+
+    static async cadastrarAve(ave: Ave): Promise<any> {
+        try {
+            let insertResult = false;
+            await database.query(`INSERT INTO ave (nome, idade, genero, envergadura)
+                VALUES
+                ('${ave.getNome().toUpperCase()}', ${ave.getIdade()}, '${ave.getGenero().toUpperCase()}', ${ave.getEnvergadura()});
+            `).then((result) => {
+                if(result.rowCount != 0) {
+                    insertResult = true;
+                }
+            });
+            return insertResult;
+        } catch(error) {
+            return error;
+        }
     }
 }
--- src/model/Mamifero.ts
+++ src/model/Mamifero.ts
@@ -1,22 +1,38 @@
 import { Animal } from "./Animal";
+import { DatabaseModel } from "./DatabaseModel";
 
+const database = new DatabaseModel().pool;
+
+/**
+ * Representa um mamífero no zoológico, uma subclasse de Animal.
+ */
 export class Mamifero extends Animal {
-    private raca: string; // Raça do mamífero
+    
+    /**
+     * A raça do mamífero.
+     */
+    private raca: string;
 
     /**
-     * Construtor da classe Mamifero.
+     * Cria uma nova instância de Mamifero.
+     * 
      * @param _raca A raça do mamífero.
      * @param _nome O nome do mamífero.
      * @param _idade A idade do mamífero.
      * @param _genero O gênero do mamífero.
      */
-    constructor(_raca: string, _nome: string, _idade: number, _genero: string) {
+    constructor(_raca: string,
+                _nome: string,
+                _idade: number,
+                _genero: string) {
+        // Chamada ao construtor da classe pai (Animal) para definir nome, idade e gênero
         super(_nome, _idade, _genero);
         this.raca = _raca;
     }
 
     /**
-     * Retorna a raça do mamífero.
+     * Obtém a raça do mamífero.
+     * 
      * @returns A raça do mamífero.
      */
     public getRaca(): string {
@@ -25,9 +41,46 @@ export class Mamifero extends Animal {
 
     /**
      * Define a raça do mamífero.
-     * @param raca A nova raça para o mamífero.
+     * 
+     * @param raca A raça a ser atribuída ao mamífero.
      */
     public setRaca(raca: string): void {
         this.raca = raca;
+    }
+
+    static async listarMamiferos() {
+        const listaDeMamiferos: Array<Mamifero> = [];
+        try {
+            const queryReturn = await database.query(`SELECT * FROM mamifero`);
+            queryReturn.rows.forEach(mamifero => {
+                listaDeMamiferos.push(mamifero);
+            });
+
+            // só pra testar se a lista veio certa do banco
+            console.log(listaDeMamiferos);
+
+            return listaDeMamiferos;
+        } catch (error) {
+            console.log('Erro no modelo');
+            console.log(error);
+            return "error";
+        }
+    }
+
+    static async cadastrarMamifero(mamifero: Mamifero): Promise<any> {
+        try {
+            let insertResult = false;
+            await database.query(`INSERT INTO mamifero (nome, idade, genero, raca)
+                VALUES
+                ('${mamifero.getNome().toUpperCase()}', ${mamifero.getIdade()}, '${mamifero.getGenero().toUpperCase()}', '${mamifero.getRaca().toUpperCase()}');
+            `).then((result) => {
+                if(result.rowCount != 0) {
+                    insertResult = true;
+                }
+            });
+            return insertResult;
+        } catch(error) {
+            return error;
+        }
     }
 }
